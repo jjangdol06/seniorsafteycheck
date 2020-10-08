f64bc3a20e6a8b746d4408c5fd06591ad4c449fc
김서영 <ksyt98@naver.com>
2020-10-08 23:25:07 +0900

    sy: finish mypage

--- backend/routes/api/user/index.js
+++ backend/routes/api/user/index.js
@@ -2,6 +2,6 @@ const router = require('express').Router()
 const controller = require('./user.controller')
 const { isLoggedIn } = require('../middlewares');
 
-router.get('/', isLoggedIn, controller.mypage);
+router.get('/', isLoggedIn, controller.mypage); // 마이페이지
 
 module.exports = router
