acb320aca7150a22506b0554c3c8ee18011dde32
jjangdol06 <jiyeon971023@gmail.com>
2020-10-19 21:34:41 +0900

    .gitignore

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Dashboard from './components/Dashboard';
 import NavbarTemplate from './components/NavbarTemplate';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   return (
