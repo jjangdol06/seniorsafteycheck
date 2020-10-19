84d899c9274d861c6bb3bce833eea92e835cfd61
Seoyoung Kim <ksyt98@gmail.com>
2020-10-19 21:20:25 +0900

    sy: finish getprogress

--- backend/routes/api/manage-progress/manage-progress.controller.js
+++ backend/routes/api/manage-progress/manage-progress.controller.js
@@ -4,40 +4,45 @@ const sequelize = require('sequelize');
 const Op = sequelize.Op;
 
 exports.getprogress = async (req, res, next) => {
-    let name_flag_mapping = new Map();
+    let id_flag_mapping = new Map();
     let flag = 0;
 
-    let today = moment(new Date()).format('YYYY-MM-DD'); // 오늘 날짜 '2020-10-12'으로 표현
+    // 오늘
+    let today = new Date();
+    today.setHours(0, 0, 0, 0); // 시간/분/초/밀리초를 00:00:00:00 으로 지정
+
+    // 오늘+1
+    let tomorrow = new Date();
+    tomorrow.setHours(0, 0, 0, 0);
+    tomorrow.setDate(tomorrow.getDate() + 1);
 
     try {
-        // const ex = await Safetycheck.findAll({ where : { socialworker_idsocialworker: req.user.idsocialworker, createdAt: {
-        //     like: "%" + today + "%" } } });
+        // 1명의 사회복지사가 오늘 방문 관리해야하는 safetycheck filtering
+        const ex = await Safetycheck.findAll({ 
+            where : { 
+                socialworker_idsocialworker: req.user.idsocialworker, 
+                service_idservice: 3,
+                createdAt: {
+                    [Op.gte]: today,
+                    [Op.lt]: tomorrow
+                  }
+            },
+        });
 
-        // const ex = await Safetycheck.findAll({ where : { socialworker_idsocialworker: req.user.idsocialworker, createdAt: {
-        //     lt: new Date() } } });
-
-        const ex = await Safetycheck.findAll({ where : { socialworker_idsocialworker: req.user.idsocialworker } }); // TODO: Date filtering
-        // 1명의 사회복지사가 오늘 관리해야하는 safetycheck
-
-        console.log('ex.length:'+ex.length);
-
-        // 1명의 사회복지사가 관리하는 senior 대상으로
+        // completed를 기준으로 나눈다.
         for ( var i = 0; i < ex.length; i++ ) {
-            if ( ex[i].service_idservice == 1 && ex[i].completed == true ) { // 오늘 아직 방문을 하지 않았다면
+            if ( ex[i].completed == false ) { // 오늘 아직 방문을 하지 않았다면
                 flag = 0; // left에 display
             } else { 
                 flag = 1; // right에 display
             }
 
-            name_flag_mapping.set(ex[i].senior_idsenior, flag);
+            id_flag_mapping.set(ex[i].senior_idsenior, flag);
         }
 
-        // test
-        for (let key of name_flag_mapping.keys()) {
-            console.log(key);
-        }
+        id_flag_mapping = Object.fromEntries(id_flag_mapping); // mapping 값 전달하기 위함
 
-        return res.send({ name_flag_mapping : name_flag_mapping }); // TODO: 전달 값 변경
+        return res.send({ id_flag_mapping : id_flag_mapping });
     } catch(error) {
         console.error(error);
         next(error);
