e85bc390e62389302d3614e67548076fc6386f9d
jjangdol06 <jiyeon971023@gmail.com>
2020-10-23 00:37:00 +0900

    update management api

--- backend/routes/api/management/index.js
+++ backend/routes/api/management/index.js
@@ -4,8 +4,8 @@ const controller = require('./management.controller')
 // mainpage : piechart
 router.get('/seniorlist', controller.seniorlist) 
 router.get('/seniordetail/:idsenior', controller.seniordetail)
-router.get('/safetycheck', controller.daysafetycheck)
-router.get('/safetycheck/:idsenior', controller.getsafetycheck)
+router.get('/safetycheck', controller.daysafetycheck) // 이미 썼어
+router.get('/safetylist', controller.getsafetycheck) // personal info 에서 사용되는 api
 router.post('/safetycheck/:idsenior', controller.postsafetycheck)
 
 module.exports = router
