8a72e53f458ebeba5165be8eeb0f660a5fd24a87
smartshk <smartshk98@gmail.com>
2020-10-22 11:42:20 +0900

    add App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import Dashboard from './components/Dashboard';
+import Management from './components/Management';
 import NavbarTemplate from './components/NavbarTemplate';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { Route, Link, Switch, BrowserRouter } from 'react-router-dom';
 
 const App = () => {
   return (
     <div>
-      <NavbarTemplate/>
-      <Dashboard/>
+      <BrowserRouter>
+        <NavbarTemplate/>
+        <Switch>
+          <Route exact path="/" component={Dashboard}/>
+          <Route path="/management" component = {Management}/>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 };
