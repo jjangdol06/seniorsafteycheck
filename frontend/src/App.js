786c5a4c2add021dc7402e2332efc8578d900a12
jjangdol06 <jiyeon971023@gmail.com>
2020-10-23 14:47:57 +0900

    fix conflict

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,11 +6,7 @@ import LoginTemplate from './components/LoginTemplate';
 import SigninInsert from './components/SigninInsert';
 import SigninTemplate from './components/SigninTemplate';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-<<<<<<< HEAD
-import { Route, Switch, BrowserRouter } from 'react-router-dom';
-=======
 import { Route, Link, Switch, BrowserRouter } from 'react-router-dom';
->>>>>>> 69cf986fe0eeafd604a0ef523a8825a280d176a7
 
 const App = () => {
   return (
