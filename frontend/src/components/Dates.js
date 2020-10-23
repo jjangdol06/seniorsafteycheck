cb9e84fb5f54116d90ac3cd5ef2d7f1dbbdc00c7
jjangdol06 <jiyeon971023@gmail.com>
2020-10-23 14:47:24 +0900

    update dates

--- frontend/src/components/Dates.js
+++ frontend/src/components/Dates.js
@@ -1,39 +1,53 @@
-import React, { useState } from "react";
-import DatePicker, {CalendarContainer} from "react-datepicker";
-import { registerLocale, setDefaultLocale } from  "react-datepicker";
+import React, { Component, useState } from "react";
+import DatePicker, { CalendarContainer } from "react-datepicker";
+import { registerLocale, setDefaultLocale } from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { CustomInput, Form, FormGroup, Label, Input } from 'reactstrap';
+import moment from 'moment'
 
 import ko from 'date-fns/locale/ko';
 registerLocale('ko', ko);
 
-const MyContainer = ({className, children}) => {
+const MyContainer = ({ className, children }) => {
     return (
         <div>
             <CalendarContainer className={className}>
                 <div style={{ background: "#f0f0f0", display: "flex", textAlign: "center" }}>
-                <FormGroup style={{ margin: "0px"}}>
-                    <CustomInput type="radio" id="exampleCustomRadio" name="customRadio" label="오늘" />
-                    <CustomInput type="radio" id="exampleCustomRadio2" name="customRadio" label="어제" />
-                </FormGroup>
+                    <FormGroup style={{ margin: "0px" }}>
+                        <CustomInput type="radio" id="exampleCustomRadio" name="customRadio" label="오늘" />
+                        <CustomInput type="radio" id="exampleCustomRadio2" name="customRadio" label="어제" />
+                    </FormGroup>
                 </div>
-                <div style={{ position: "relative", padding: "0px"}}>{children}</div>
+                <div style={{ position: "relative", padding: "0px" }}>{children}</div>
             </CalendarContainer>
         </div>
     );
 };
 
-const Dates = () => {
-    const [startDate, setStartDate] = useState(new Date());
-    return (
-        <DatePicker
-            dateFormat="yyyy.MM.dd(eee)"
-            locale="ko"
-            selected={startDate} 
-            onChange={date => setStartDate(date)}
-            calendarContainer={MyContainer}
-        />
-    );
+class Dates extends Component{
+
+    constructor(props) {
+        super(props)
+        this.setStartDate = this.setStartDate.bind(this);
+        this.startDate = new Date()
+    }
+
+    setStartDate(date) {
+        this.startDate = date
+        console.log(date)
+    }
+    
+    render() {
+        return (
+            <DatePicker
+                dateFormat="yyyy.MM.dd(eee)"
+                locale="ko"
+                selected={this.startDate}
+                onChange={date => this.setStartDate(date)}
+                calendarContainer={MyContainer}
+            />
+        )
+    }
 }
 
 export default Dates;
